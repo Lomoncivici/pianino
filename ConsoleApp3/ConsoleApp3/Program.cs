bef61ae7ede54de1e32a24b39307f95952086567
Lomoncivici <125694971+Lomoncivici@users.noreply.github.com>
2023-10-10 19:49:14 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection.PortableExecutable;
+
+while (true)
+{
+    ConsoleKeyInfo sound = Console.ReadKey();
+
+    int[] massive1 = new[] { 1635, 1732, 1835, 1945, 2060, 2183, 2312, 2450, 2596, 2750, 2914, 3087 };
+    int[] massive2 = new[] { 3270, 3465, 3671, 3889, 4120, 3465, 4625, 4900, 5191, 5500, 5827, 6174 };
+    int[] tonalnasti = new[] {0};
+
+    if (sound.Key == ConsoleKey.F1)
+    {
+            tonalnasti = new int[massive1.Length];
+            massive1.CopyTo(tonalnasti, 0);
+
+            Console.WriteLine(String.Join(", ", tonalnasti));
+    }
+    else if (sound.Key == ConsoleKey.F2)
+    {
+        tonalnasti = new int[massive2.Length];
+        massive2.CopyTo(tonalnasti, 0);
+
+        Console.WriteLine(String.Join(", ", tonalnasti));
+    }
+
+    while (true)
+    {
+        if (sound.Key == ConsoleKey.A)
+        {
+            Console.Beep(tonalnasti[0], 100);
+        }
+        else if (sound.Key == ConsoleKey.W)
+        {
+            Console.Beep(tonalnasti[1], 100);
+        }
+        else if (sound.Key == ConsoleKey.S)
+        {
+            Console.Beep(tonalnasti[2], 100);
+        }
+        else if (sound.Key == ConsoleKey.E)
+        {
+            Console.Beep(tonalnasti[3], 100);
+        }
+        else if (sound.Key == ConsoleKey.D)
+        {
+            Console.Beep(tonalnasti[4], 100);
+        }
+        else if (sound.Key == ConsoleKey.R)
+        {
+            Console.Beep(tonalnasti[5], 100);
+        }
+        else if (sound.Key == ConsoleKey.F)
+        {
+            Console.Beep(tonalnasti[6], 100);
+        }
+        else if (sound.Key == ConsoleKey.T)
+        {
+            Console.Beep(tonalnasti[7], 100);
+        }
+        else if (sound.Key == ConsoleKey.G)
+        {
+            Console.Beep(tonalnasti[8], 100);
+        }
+        else if (sound.Key == ConsoleKey.Y)
+        {
+            Console.Beep(tonalnasti[9], 100);
+        }
+        else if (sound.Key == ConsoleKey.H)
+        {
+            Console.Beep(tonalnasti[10], 100);
+        }
+        else if (sound.Key == ConsoleKey.U)
+        {
+            Console.Beep(tonalnasti[11], 100);
+        }
+        sound = Console.ReadKey(true);
+    }
+}
